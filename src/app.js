5823f8dd100a1fa4c05bc494c9b8aa21e3f8aaa0
Simeon Vincent <svincent@gmail.com>
2014-05-04 13:59:56 -0700

    Stub out Jade view

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('Hello World');
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade');
+
+
+app.get('/', function (req, res) {
+	res.render('index', {
+		title : 'Home'
+	});
 });
 
 var server = app.listen(process.env.PORT, function() {
-    console.log('Listening on port %d', server.address().port);
+	console.log('Listening on port %d', server.address().port);
 });
