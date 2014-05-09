873667ea1465d6663fdebf6c114a58f51c818137
Christopher Giroir <kelsin@valefor.com>
2014-05-08 23:48:22 -0700

    Adding coverage for issues skeleton controller

---
+++ test/integration/issues.js
@@ -0,0 +1,24 @@
+var app = require('../../src/app.js');
+
+var chai = require('chai');
+var supertestChai = require('supertest-chai');
+var request = supertestChai.request;
+
+chai.should();
+chai.use(supertestChai.httpAsserts);
+
+describe('/issues', function() {
+  it('should return 200', function(done) {
+    request(app).get('/issues').end(function (res) {
+      res.should.have.status(200);
+      done();
+    });
+  });
+
+  it('should be html', function(done) {
+    request(app).get('/issues').end(function (res) {
+      res.should.be.html;
+      done();
+    });
+  });
+});
