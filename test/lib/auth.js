6bd11995b799591502dcf64ece5f800b194e1c49
Christopher Giroir <kelsin@valefor.com>
2014-05-08 23:42:09 -0700

    Adding another scope to this auth file

--- test/lib/auth.js
+++ test/lib/auth.js
@@ -4,15 +4,17 @@ var sinonChai = require('sinon-chai');
 chai.should();
 chai.use(sinonChai);
 
-var auth = require('../../src/lib/auth');
+describe('auth', function() {
+  var auth = require('../../src/lib/auth');
 
-describe('githubSuccess', function() {
-  it("should redirect to '/'", function() {
-    var res = { redirect: sinon.spy() };
+  describe('.githubSuccess()', function() {
+    it("should redirect to '/'", function() {
+      var res = { redirect: sinon.spy() };
 
-    auth.githubSuccess(null, res);
+      auth.githubSuccess(null, res);
 
-    res.redirect.should.have.been.calledOnce;
-    res.redirect.should.have.been.calledWith('/');
+      res.redirect.should.have.been.calledOnce;
+      res.redirect.should.have.been.calledWith('/');
+    });
   });
 });
