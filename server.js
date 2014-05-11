e0bdc054be61323c0bfb8b77b7454e61629fa5cc
Christopher Giroir <kelsin@valefor.com>
2014-05-11 00:09:30 -0700

    Adding error logging and handlers for 404 and 500 responses

--- server.js
+++ server.js
@@ -3,21 +3,32 @@ var express = require('express');
 var passport = require('passport');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
+var errors = require('./src/controllers/errors');
 
 var app = express();
 
-// Middleware
-app.use('/static', express.static(path.join(__dirname, 'dist', 'public')));
-app.use(cookieParser());
-app.use(session({ secret: process.env.COOKIE_SECRET }));
-app.use(passport.initialize());
-app.use(passport.session());
-
+// Config
 app.set('views', path.join(__dirname, 'src', 'views'));
 app.set('view engine', 'jade');
 
-// Routes
+// Static Content
+app.use('/static', express.static(path.join(__dirname, 'dist', 'public')));
+
+// Parse cookies and sessions
+app.use(cookieParser());
+app.use(session({ secret: process.env.COOKIE_SECRET }));
+
+// Authentication
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Normal Routes
 require('./src/routes')(app);
+
+// Error Handling
+app.use(errors.notFound);
+app.use(errors.log);
+app.use(errors.error);
 
 // Serve
 var server = app.listen(process.env.PORT, function() {
