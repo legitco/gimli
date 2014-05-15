c2d9c3b813f120238b2b6eb38b537e03d1520a99
Simeon Vincent <svincent@gmail.com>
2014-05-14 22:41:12 -0700

    Normalize spacing

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,8 +2,8 @@ var angular = angular || {};
 var gimli = angular.module('gimli', []);
 
 gimli.controller('IssuesController', ['$scope', function($scope){
-	$scope.issues = [
-		{id:1, title: "I am an issue"},
-		{id:1, title: "I have an issue"}
-	];
+  $scope.issues = [
+    {id:1, title: "I am an issue"},
+    {id:2, title: "I have an issue"}
+  ];
 }]);
