3269830b2eeb9f8f12bcf883a6cb553a5951aa2b
Christopher Giroir <kelsin@valefor.com>
2014-05-18 12:04:04 -0700

    Adding in some middleware to check login

--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,23 @@ var page = require('./controllers/page');
 var issues = require('./controllers/issues');
 var repos = require('./controllers/repos');
 
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    req.session.redirect_url = req.url;
+    res.redirect('/login');
+  }
+}
+
+function apiEnsureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    next(new Error("Not logged in"));
+  }
+}
+
 module.exports = function(app) {
   resources.init(app);
 
@@ -15,14 +32,17 @@ module.exports = function(app) {
   app.get('/login', auth.githubLogin);
   app.get('/auth/github/callback', auth.githubCallback, auth.githubSuccess);
 
+  // Make sure we're logged in first
+  app.use(ensureAuthenticated);
+
   // Repos
-  app.get('/api/repos', repos.index);
-  app.post('/api/repos/:owner/:repo/subscribe', repos.subscribe);
+  app.get('/api/repos', apiEnsureAuthenticated, repos.index);
+  app.post('/api/repos/:owner/:repo/subscribe', apiEnsureAuthenticated, repos.subscribe);
 
   // Issues
-  app.get('/api/:owner/:repo/issues', issues.index);
-  app.get('/api/:owner/:repo/issue/:number', issues.show);
-  app.get('/api/:owner/:repo/issue/:number/comments', issues.comments);
+  app.get('/api/:owner/:repo/issues', apiEnsureAuthenticated, issues.index);
+  app.get('/api/:owner/:repo/issue/:number', apiEnsureAuthenticated, issues.show);
+  app.get('/api/:owner/:repo/issue/:number/comments', apiEnsureAuthenticated, issues.comments);
 
   // Github Notifications
   app.post('/notice/issue', issues.notice);
