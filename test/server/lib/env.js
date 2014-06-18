34329692d08085240bfd8aa9f6b2cf5f251f916b
Simeon Vincent <svincent@gmail.com>
2014-06-17 19:31:23 -0700

    Fix incorrect bind syntax in env tests

--- test/server/lib/env.js
+++ test/server/lib/env.js
@@ -23,43 +23,27 @@ describe('Env', function() {
 
     it('should throw an error if called with no param, a Number, a string, or an object', function(){
       expect(method).to.throw(Error);
-      expect(method.bind(1)).to.throw(Error);
-      expect(method.bind("String test")).to.throw(Error);
-      expect(method.bind({biz: "baz"})).to.throw(Error);
+      expect(method.bind(method, 1)).to.throw(Error);
+      expect(method.bind(method, "String test")).to.throw(Error);
+      expect(method.bind(method, {biz: "baz"})).to.throw(Error);
     });
 
     it('should throw an error when called with an invalid array', function(){
-      expect(method.bind([1])).to.throw(Error);
-      expect(method.bind([{fizz: "buzz"}])).to.throw(Error);
+      expect(method.bind(method, [1])).to.throw(Error);
+      expect(method.bind(method, [{fizz: "buzz"}])).to.throw(Error);
     });
 
     it('should throw an error when called with an unset env var', function(){
-      expect(method.bind(["THIS_IS_NOT_VALID"])).to.throw(Error);
-      expect(method.bind(["ABCDEFGHIJKLMNOPQRSTUVWXYZ"])).to.throw(Error);
-      expect(method.bind(["PWD", "INVALID_ENV_VAR"])).to.throw(Error);
+      expect(method.bind(method, ["THIS_IS_NOT_VALID"])).to.throw(Error);
+      expect(method.bind(method, ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"])).to.throw(Error);
+      expect(method.bind(method, ["PWD", "INVALID_ENV_VAR"])).to.throw(Error);
     });
 
     it('should match "PWD" and return true', function(){
+      expect(method.bind(method, ["PWD"])).to.not.throw(Error);
       expect(method(["PWD"])).to.be.true;
     });
   });
-
-  // describe('.validateInput()', function(){
-  //   var methodName = 'validateInput';
-  //   var method = (new module())[methodName];
-
-  //   it('should be a method', function(){
-  //     expect(module).to.respondTo(methodName);
-  //     expect(module).itself.not.to.respondTo(methodName);
-  //   });
-
-  //   it('should throw if not provided an array of strings', function(){
-  //     expect(method).to.throw(Error);
-  //     expect(method.bind(method, [])).to.throw(Error);
-  //     expect(method.bind(method, ['test', 1])).to.throw(Error);
-  //     expect(method.bind(method, ['test', 'again'])).to.not.throw(Error);
-  //   });
-  // });
 
   // it('', function(){});
   // describe('', function(){});
