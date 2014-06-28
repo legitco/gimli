e90fbeb6c4fca715594452c0753d18551c458e75
Christopher Giroir <kelsin@valefor.com>
2014-06-27 21:30:31 -0700

    Using mongoose instead of redis for Users

--- server/models/user.js
+++ server/models/user.js
@@ -1,50 +1,14 @@
-var redis = require('../lib/redis');
+var mongoose = require('mongoose');
 
-var key = function(id, name) {
-  return('gimli:user:' + id + ':' + name);
-};
+var userSchema = new mongoose.Schema({
+  id: String,
+  name: String,
+  login: String,
+  html_url: String,
+  avatar_url: String,
+  email: String,
+  location: String,
+  access: String
+});
 
-var Model = function() {};
-Model.prototype.load = function(id, callback) {
-  if (id !== null && typeof id !== "undefined") {
-    redis.multi()
-      .get(key(id, "name"))
-      .get(key(id, "login"))
-      .get(key(id, "html_url"))
-      .get(key(id, "avatar_url"))
-      .get(key(id, "email"))
-      .get(key(id, "location"))
-      .get(key(id, "access"))
-      .exec(function(err, replies) {
-        callback({
-          id: id,
-          name: replies[0],
-          login: replies[1],
-          html_url: replies[2],
-          avatar_url: replies[3],
-          email: replies[4],
-          location: replies[5],
-          access: replies[6]
-        });
-      });
-  } else {
-    callback(null);
-  }
-};
-
-Model.prototype.save = function(user, access, done) {
-  var id = user._json.id;
-  redis.multi()
-    .set(key(id, 'name'), user._json.name)
-    .set(key(id, 'login'), user._json.login)
-    .set(key(id, 'html_url'), user._json.html_url)
-    .set(key(id, 'avatar_url'), user._json.avatar_url)
-    .set(key(id, 'email'), user._json.email)
-    .set(key(id, 'location'), user._json.location)
-    .set(key(id, 'access'), access)
-    .exec(function(err, replies) {
-      done();
-    });
-};
-
-module.exports = new Model();
+module.exports = mongoose.model('User', userSchema);
--- test/server/models/user.js
+++ test/server/models/user.js
@@ -1,77 +1,12 @@
+var mongooseMock = require('mongoose-mock');
+var proxyquire = require('proxyquire');
 var chai = require('chai');
 var redis = require('../../../server/lib/redis');
 chai.should();
 
 describe('user', function() {
-  var user = require('../../../server/models/user');
-
-  var githubProfile = {
-    _json: {
-      id: 5,
-      name: "Test User",
-      login: "testuser"
-    }
-  };
-
-  // Clear redis on each run
-  beforeEach(function(done) {
-    redis.flushdb(done);
-  });
-
-  describe('.save()', function() {
-    beforeEach(function(done) {
-      user.save(githubProfile, 'token', done);
-    });
-
-    it("should save name to redis", function() {
-      redis.get('gimli:user:5:name', function(err, reply) {
-        (reply === null).should.be.false;
-        reply.should.equal("Test User");
-      });
-    });
-
-    it("should save login to redis", function() {
-      redis.get('gimli:user:5:login', function(err, reply) {
-        (reply === null).should.be.false;
-        reply.should.equal("testuser");
-      });
-    });
-
-    it("should save the access token to redis", function() {
-      redis.get('gimli:user:5:access', function(err, reply) {
-        (reply === null).should.be.false;
-        reply.should.equal("token");
-      });
-    });
-  });
-
-  describe('.load()', function() {
-    beforeEach(function(done) {
-      user.save(githubProfile, 'token', done);
-    });
-
-    it("should return null if asked to load null", function(done) {
-      user.load(null, function(loadedUser) {
-        (loadedUser === null).should.be.true;
-        done();
-      });
-    });
-
-    it("should return null if asked to load undefined", function(done) {
-      user.load(undefined, function(loadedUser) {
-        (loadedUser === null).should.be.true;
-        done();
-      });
-    });
-
-    it("should retrieve all data on load", function(done) {
-      user.load(5, function(loadedUser) {
-        loadedUser.id.should.equal(5);
-        loadedUser.name.should.equal("Test User");
-        loadedUser.login.should.equal("testuser");
-        loadedUser.access.should.equal("token");
-        done();
-      });
-    });
+  it('should not be null', function() {
+    var User = proxyquire('../../../server/models/user', { 'mongoose': mongooseMock });
+    User.should.not.be.null;
   });
 });
