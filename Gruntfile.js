93f1b5b82905720dd0137f9ed6349a57393f1103
Matthew Schnee <matthew.schnee@gmail.com>
2014-07-19 17:20:09 -0700

    Changes concat to concat-sourcemap, so js sourcemaps

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-stylus');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-concat-sourcemap');
   grunt.loadNpmTasks('grunt-contrib-copy');
 
   grunt.loadNpmTasks('grunt-karma');
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
+    concat_sourcemap: {
       options: {
         separator: ';'
       },
@@ -221,7 +221,7 @@ module.exports = function(grunt) {
   grunt.registerTask('ci',                ['watch:test']);
 
   // How to build
-  grunt.registerTask('build',             ['jshint', 'concat', 'copy', 'stylus']);
+  grunt.registerTask('build',             ['jshint', 'concat_sourcemap', 'copy', 'stylus']);
   grunt.registerTask('heroku:production', ['build']);
 
   // How to run
