4c1b4365ef0536b6b7f3d98dce68d55675d9a557
Christopher Giroir <kelsin@valefor.com>
2014-10-11 15:50:00 -0700

    Working gimli deploys

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,12 +3,11 @@ lock '3.2.1'
 
 set :application, 'gimli'
 set :repo_url, 'git@github.com:legitco/gimli.git'
+set :npm_flags, '--silent'
 set :grunt_tasks, 'build'
-set :grunt_file, -> { release_path.join('Gruntfile.js') }
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-set :branch, 'develop'
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/home/app/www/gimli'
